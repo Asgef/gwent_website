f109d689ed386eb9cc65be2de0ca9807e792bbb4
Asgef <asgefes1@gmail.com>
2024-06-18 16:32:21 +0300

    Fix settings

--- game_gwent/settings.py
+++ game_gwent/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-PRODUCTION = os.getenv('PRODUCTION', 'False') == 'True'
+PRODUCTION = os.getenv('PRODUCTION', False) == 'True'
 
 ALLOWED_HOSTS = ['*']
 
