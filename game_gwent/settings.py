16dd91e5c62012948cec5b2dce52c3278cc5820d
Asgef <asgefes1@gmail.com>
2024-06-18 17:06:49 +0300

    Fix settings

--- game_gwent/settings.py
+++ game_gwent/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False) == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-PRODUCTION = os.getenv('PRODUCTION', False) == 'True'
+PRODUCTION = os.getenv('PRODUCTION', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
